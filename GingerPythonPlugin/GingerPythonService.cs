740b99871afb586bb61efe4d358ff3eb2a97406b
Hassidi, Ronen (rh0016) <rh0116@us.att.com>
2018-09-10 16:40:13 -0500

    Add project files.

---
+++ GingerPythonPlugin/GingerPythonService.cs
@@ -0,0 +1,17 @@
+﻿using Amdocs.Ginger.Plugin.Core;
+using System;
+
+namespace GingerPythonPlugin
+{
+    [GingerService("SHELL", "Shell Server")]
+    public class GingerPythonService : IGingerService, IStandAloneAction
+    {
+
+        [GingerAction("RunPython", "Run Python Command")]
+        public void RunPython(IGingerAction GA, string PythonFile)
+        {
+            string retOutput;
+
+        }
+    }
+}
